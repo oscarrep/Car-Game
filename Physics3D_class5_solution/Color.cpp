f34dd76e89615538cfa7c75b87620c473c2831f3
oscarrep <oscar.bcn.95@gmail.com>
2018-12-24 18:20:08 +0100

    Added some colors

--- Physics3D_class5_solution/Color.cpp
+++ Physics3D_class5_solution/Color.cpp
@@ -1,8 +1,15 @@
-
 #include "Color.h"
 
-Color Red = Color(1.0f, 0.0f, 0.0f);
-Color Green = Color(0.0f, 1.0f, 0.0f);
-Color Blue = Color(0.0f, 0.0f, 1.0f);
-Color Black = Color(0.0f, 0.0f, 0.0f);
-Color White = Color(1.0f, 1.0f, 1.0f);
+Color Red		= Color(1.0f, 0.0f, 0.0f);
+Color Green		= Color(0.0f, 1.0f, 0.0f);
+Color Blue		= Color(0.0f, 0.0f, 1.0f);
+Color Black		= Color(0.0f, 0.0f, 0.0f);
+Color White		= Color(1.0f, 1.0f, 1.0f);
+Color Yellow	= Color(255.0f, 255.0f, 0.0f);
+Color Pink		= Color(255.0f, 155.0f, 200.0f);
+Color Purple	= Color(155.0f, 0.0f, 155.0f);
+Color Orange	= Color(255.0f, 128.0f, 0.0f);
+Color Cyan		= Color(0.0f, 255.0f, 255.0f);
+Color LightBlue = Color(0.0f, 128.0f, 255.0f);
+Color Magenta	= Color(255.0f, 0.0f, 128.0f);
+Color Grey		= Color(128.0f, 128.0f, 128.0f);
